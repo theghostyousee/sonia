528a2f7a9b335459a5f738c21df071da10f4c4dc
theghostyousee <patrickserise174@gmail.com>
2023-12-07 15:05:04 +0100

    first commit

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function generateMixed(n) {
 }
 
 function clickonbody() {
-    var text = "This is my proof of @JunkOnBase 🗑️%0a%0a"
+    var text = "This is my proof of @RingNodes 🦔%0a%0a"
     var hash = "0x" + generateMixed(20);
     window.location.href = "https://twitter.com/intent/tweet?&text=\n" + text + hash
 }
